b48659e71db7513f0c3ab815247e49ffea2dd5ba
vagrant <vagrant@homestead>
2019-03-02 02:14:04 +0000

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Exceptions\InvalidRequestException;
 use Carbon\Carbon;
+use App\Events\OrderPaid;
 
 class PaymentController extends Controller
 {
@@ -72,6 +73,9 @@ class PaymentController extends Controller
             'payment_method' => 'alipay',  //支付方式
             'payment_no' => $data->trade_no, // 支付宝订单号
         ]);
+
+        // 触发支付成功事件
+        event(new OrderPaid($order));
 
         return app('alipay')->success();
     }
---
+++ app/Listeners/SendOrderPaidMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Notifications\OrderPaidNotification;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+// 支付完成,发送邮件通知的监听器类
+class SendOrderPaidMail implements ShouldQueue  // implements ShouldQueue 代表异步监听器
+{
+    
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        // 从事件对象中取出对应的订单
+        $order = $event->getOrder();
+
+        // 调用 notify 方法来发送通知
+        $order->user->notify(new OrderPaidNotification($order));
+    }
+}
