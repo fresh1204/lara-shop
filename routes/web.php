ad5ccd054c70452d397b4cc06dda8cedad38daab
vagrant <vagrant@homestead>
2019-03-03 11:13:36 +0000

    用户界面 - 检查优惠券

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,8 @@ Route::group(['middleware' => ['auth','verified']],function(){
 	//提交申请退款
 	Route::post('orders/{order}/apply_refund','OrdersController@applyRefund')->name('orders.apply_refund');
 
-
+	// 检查优惠券
+	Route::get('coupon_codes/{code}','CouponCodesController@show')->name('coupon_codes.show');
 });
 
 Route::get('products','ProductsController@index')->name('products.index');
